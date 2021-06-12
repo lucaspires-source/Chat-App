3c1e5522a01366c3cd66b2dd1e0f5b6db358fb3a
lucaspires-source <pires.lucas94@gmail.com>
2021-06-12 12:21:43 -0300

    finished working on chatfeed

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,6 +1,15 @@
 
 
-const MyMessage = () => {
+const MyMessage = ({message}) => {
+    if(message?.attachments?.length > 0){
+        return(
+            <img src={message.attachments[0].file}
+            alt="message"
+            className="message-image"
+            style={{float:'right'}}
+            />
+        )
+    }
     return (
         <div>
             My MyMessage
